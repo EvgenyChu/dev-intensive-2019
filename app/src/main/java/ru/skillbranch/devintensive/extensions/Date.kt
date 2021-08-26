cf4792ffc74a6dbe590fab6624320c645384b42b
EvgenyChu <79277702141@ya.ru>
2021-08-26 09:43:34 +0300

    "lektion"

---
+++ app/src/main/java/ru/skillbranch/devintensive/extensions/Date.kt
@@ -0,0 +1,51 @@
+package ru.skillbranch.devintensive.extensions
+
+import java.lang.IllegalStateException
+import java.text.SimpleDateFormat
+import java.util.*
+
+const val SECOND = 1000L
+const val MINUTE = 60 * SECOND
+const val HOUR = 60 * MINUTE
+const val DAY = 24 * HOUR
+
+
+
+fun Date.format(pattern:String="HH:mm:ss dd.MM.yy"):String {
+    val dateFormat = SimpleDateFormat(pattern, Locale("ru"))
+    return dateFormat.format(this)
+}
+
+fun Date.add(value:Int, units: TimeUnits = TimeUnits.SECOND) : Date {
+    var time = this.time
+
+    time += when (units) {
+        TimeUnits.SECOND -> value * SECOND
+        TimeUnits.MINUTE -> value * MINUTE
+        TimeUnits.HOUR -> value * HOUR
+        TimeUnits.DAY -> value * DAY
+    }
+    this.time = time
+
+    return this
+}
+fun Date.humanizeDiff(date:Date = Date() /*it is current date*/ ) : String{
+    this //this id receiver date (User.lastVisit.humanizeDiff)
+    val lastVisitTime = this.time //this is time in milliseconds last visit
+    val timeDiff = date.time - lastVisitTime//difference between now time and lastVisit time
+    return when(timeDiff){
+        in 0 * SECOND..1 * SECOND -> "только что" //0 .. 1000 milliseconds [0..1000] // equivalent of 0 <= timeDiff && timeDiff <= 1000
+        in (1 * SECOND).inc()..45 * SECOND -> "несколько секунд назад" //1001 ..45 000 milliseconds [1001 .. 45000]
+        // .inc() this is increment (+1);  example : 9.inc() == 10
+        in (45 * SECOND).inc()..600 * SECOND -> "несколько минут назад"
+
+        else -> "более года назад"
+    }
+}
+
+    enum class TimeUnits{
+      SECOND,
+      MINUTE,
+      HOUR,
+      DAY
+    }
